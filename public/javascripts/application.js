70400fd5cf7715923d52d951bbcd64bf5392b523
xurde <jorgedf@gmail.com>
2009-01-27 04:49:59 +0100

    forms empty on focus fixed

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+// Forms tagged with class "emptyme" are emptied on first focus
+$(document).ready(function() {
+	$(".emptyme").bind("focus", function() {
+		$(this).val("");
+		//$(this).removeClass("emptyme")
+		$(this).unbind("focus")
+	})
+});
