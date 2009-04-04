c2aa70af7152e70d232876ea2855963ab6d3c5ea
Jorge Díaz <jorgedf@gmail.com>
2009-04-05 00:26:57 +0200

    answer button disabled until form focus

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,9 +2,25 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 // Forms tagged with class "emptyme" are emptied on first focus
+// $(document).ready(function() {
+// 	$(".emptyme").bind("focus", function() {
+// 		$(this).val("");
+// 		$(this).unbind("focus");
+// 	})
+// });
+// 
+// 
+// $("#myForm").bind("keypress", function(e) {
+//  if (e.keyCode == 34) return false;
+// });
+
 $(document).ready(function() {
-	$(".emptyme").bind("focus", function() {
-		$(this).val("");
-		$(this).unbind("focus")
-	})
+
+  $(".emptyme").bind("focus", function() {
+     $(this).val("");
+     var $submitButton = $(this).next();
+     $submitButton.removeAttr("disabled");
+
+     $(this).unbind("focus")
+   })
 });
